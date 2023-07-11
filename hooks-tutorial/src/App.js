1a3eff488aba0b84842463d3d92aa091b65d6abd
Jeong-Je <jeongjeson656@gmail.com>
2023-07-11 23:10:59 +0900

    first commit

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Counter from './Counter';
+import Info from './Info';
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <Counter />
+      <div>
+        <button onClick={()=>{setVisible(!visible)}}>{visible ? '숨기기':'보이기'}</button>
+        <hr />
+        {visible && <Info/>}
+      </div>
+    </div>
+  )
+}
+export default App;
+
